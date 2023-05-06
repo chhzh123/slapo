5a61312f6b377ebeca413edeba3e72fa87dedd3f
chhzh123 <hc676@cornell.edu>
2023-05-06 20:39:39 +0000

    Fix comm cost

--- playground/test_mlp.py
+++ playground/test_mlp.py
@@ -16,7 +16,6 @@ p = 2
 
 bitvecs = {}
 
-ops = ["matmul", "matmul_1"]
 
 class Spec:
     def __init__(self, spec):
@@ -37,21 +36,34 @@ class Spec:
     def __str__(self):
         return self.spec
 
-output_map = {"RR": "RS", "RS": "RR", "SR": "SR"}
 
 class Matmul:
 
-    @staticmethod
-    def generate_output(lhs, rhs):
-        return Spec(output_map[Spec(lhs).spec]).id
-    
-    @staticmethod
-    def generate_output_z3(lhs, rhs):
+    def __init__(self, name, m, n):
+        self.name = name
+        self.shape = m * n
+        self.output_map = {"RR": "RS", "RS": "RR", "SR": "SR"}
+        self.comm_cost_map = {"RR": 0, "RS": self.shape, "SR": 0}
+
+    def generate_output(self, lhs, rhs):
+        return Spec(self.output_map[Spec(lhs).spec]).id
+
+    def generate_output_z3(self, lhs, rhs):
         result = 3 # invalid
-        for inp, out in output_map.items():
+        for inp, out in self.output_map.items():
             result = z3.If(lhs == Spec(inp).id, Spec(out).id, result)
         return result
 
+    def calculate_comm_cost(self, lhs, rhs):
+        return self.comm_cost_map[Spec(lhs).spec]
+
+    def calculate_comm_cost_z3(self, lhs, rhs):
+        result = 10e8 # invalid
+        for inp, cost in self.comm_cost_map.items():
+            result = z3.If(lhs == Spec(inp).id, cost, result)
+        return result
+
+ops = [Matmul("matmul", M, K), Matmul("matmul_1", K, N)]
 
 input_constraints = []
 format_constraints = []
@@ -59,10 +71,11 @@ outputs = []
 comm_costs = []
 for op in ops:
     # no need to include output, since output can be obtained from inputs
-    lhs = z3.BitVec(f"{op}_lhs", 2) # input
-    rhs = z3.BitVec(f"{op}_rhs", 2) # weight
-    bitvecs[f"{op}_lhs"] = lhs
-    bitvecs[f"{op}_rhs"] = rhs
+    name = op.name
+    lhs = z3.BitVec(f"{name}_lhs", 2) # input
+    rhs = z3.BitVec(f"{name}_rhs", 2) # weight
+    bitvecs[f"{name}_lhs"] = lhs
+    bitvecs[f"{name}_rhs"] = rhs
 
     # input constraints
     constraints = [z3.And(lhs == Spec("RR").id, rhs == Spec("RS").id),
@@ -74,14 +87,10 @@ for op in ops:
     format_constraints.extend([z3.ULE(lhs, 3), z3.ULE(rhs, 3)])
 
     # output
-    outputs.append(Matmul.generate_output_z3(lhs, rhs)) # SR x RR
+    outputs.append(op.generate_output_z3(lhs, rhs))
 
     # communication cost
-    comm_costs.append(z3.If(lhs == Spec("RR").id, # RR x RS
-                     0,
-                 z3.If(lhs == Spec("RS").id, # RS x SR
-                      M * K if op == "matmul" else K * N,
-                      0))) # SR x RR
+    comm_costs.append(op.calculate_comm_cost_z3(lhs, rhs))
 
 def calculate_reshard_cost(prev, curr):
     return z3.If(prev == Spec("RR").id,
@@ -117,16 +126,7 @@ def model_values(model):
         for d in model.decls()
     }
 
-def calculate_comm_cost(lhs, rhs):
-    if lhs == Spec("RR").id: # RR x RS
-        return 0
-    elif lhs == Spec("RS").id: # RS x SR
-        return M * K # all_reduce
-    elif lhs == Spec("SR").id:
-        return 0
-
 def calculate_reshard_cost(prev, curr):
-    print("reshard", prev, curr)
     if prev == Spec("RR").id:
         if curr == Spec("RR").id:
             return 0
@@ -162,15 +162,15 @@ for _ in range(3):
     mod = sol.model()
     print(mod)
     results = model_values(mod)
-    print(calculate_reshard_cost(Spec("RR").id, results[f"{ops[0]}_lhs"]))
     max_cost = 0
     for i, op in enumerate(ops):
-        lhs = results[f"{op}_lhs"]
-        rhs = results[f"{op}_rhs"]
-        output = Matmul.generate_output(lhs, rhs)
-        print(f"{op}: {Spec(lhs)} x {Spec(rhs)} = {Spec(output)}")
-        comm_cost = calculate_comm_cost(lhs, rhs)
-        reshard_cost = calculate_reshard_cost(output, results[f"{ops[i + 1]}_lhs"] if i < len(ops) - 1 else Spec("RR").id)
+        name = op.name
+        lhs = results[f"{name}_lhs"]
+        rhs = results[f"{name}_rhs"]
+        output = op.generate_output(lhs, rhs)
+        print(f"{name}: {Spec(lhs)} x {Spec(rhs)} = {Spec(output)}")
+        comm_cost = op.calculate_comm_cost(lhs, rhs)
+        reshard_cost = calculate_reshard_cost(output, results[f"{ops[i + 1].name}_lhs"] if i < len(ops) - 1 else Spec("RR").id)
         max_cost += comm_cost + reshard_cost
         print(comm_cost, reshard_cost)
     print("Total cost:", max_cost)
