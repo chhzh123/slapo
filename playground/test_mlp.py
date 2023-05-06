a79afc63443e8f4d636bc186c85fbbd6ed820f19
chhzh123 <hc676@cornell.edu>
2023-05-06 19:20:57 +0000

    Add cost model

--- playground/test_mlp.py
+++ playground/test_mlp.py
@@ -8,6 +8,12 @@ S: Sharded
 2: 10 SR
 """
 
+# kernel size
+M = 512
+K = 1024
+N = 1024
+p = 2
+
 bitvecs = {}
 
 ops = ["matmul", "matmul_1"]
@@ -33,6 +39,8 @@ class Spec:
 
 input_constraints = []
 format_constraints = []
+outputs = []
+comm_costs = []
 for op in ops:
     # no need to include output, since output can be obtained from inputs
     lhs = z3.BitVec(f"{op}_lhs", 2) # input
@@ -49,9 +57,47 @@ for op in ops:
     # format constraints
     format_constraints.extend([z3.ULE(lhs, 3), z3.ULE(rhs, 3)])
 
+    # output
+    outputs.append(z3.If(lhs == Spec("RR").id, # RR x RS
+                   Spec("RS").id,
+                z3.If(lhs == Spec("RS").id, # RS x SR
+                    Spec("RR").id,
+                    Spec("SR").id))) # SR x RR
+
+    # communication cost
+    comm_costs.append(z3.If(lhs == Spec("RR").id, # RR x RS
+                     0,
+                 z3.If(lhs == Spec("RS").id, # RS x SR
+                      M * K if op == "matmul" else K * N,
+                      0))) # SR x RR
+
+def calculate_reshard_cost(prev, curr):
+    return z3.If(prev == Spec("RR").id,
+        z3.If(curr == Spec("RR").id, 0,
+            z3.If(curr == Spec("RS").id, 0,
+                z3.If(curr == Spec("SR").id, 0, 0))),
+        z3.If(prev == Spec("RS").id,
+            z3.If(curr == Spec("RR").id, 1 / p * M * K,
+                z3.If(curr == Spec("RS").id, 0,
+                    z3.If(curr == Spec("SR").id, (1 / p - 1 / (p * p)) * M * K, 0))),
+            z3.If(prev == Spec("SR").id,
+                z3.If(curr == Spec("RR").id, 1 / p * M * K,
+                    z3.If(curr == Spec("RS").id, (1 / p - 1 / (p * p)) * M * K,
+                        z3.If(curr == Spec("SR").id, 0, 0))),
+                0)))
+
+reshard_costs = []
+for i in range(len(outputs)):
+    prev = outputs[i]
+    curr = outputs[i + 1] if i < len(outputs) - 1 else Spec("RR").id
+    reshard_costs.append(calculate_reshard_cost(prev, curr))
+
+cost = sum(comm_costs) + sum(reshard_costs)
+
 goal = []
 goal += input_constraints
 goal += format_constraints
+goal.append(cost < 1e10)
 
 def solve(phi):
     sol = z3.Solver()
@@ -68,7 +114,9 @@ def model_values(model):
 mod = solve(goal)
 print(mod)
 results = model_values(mod)
-for op in ops:
+for i, op in enumerate(ops):
     lhs = results[f"{op}_lhs"]
     rhs = results[f"{op}_rhs"]
+    # output = generate_output(lhs, rhs)
     print(f"{op}: {Spec(lhs)} x {Spec(rhs)}")
+    # print(calculate_comm_cost(lhs, rhs), calculate_reshard_cost(output, results[f"{ops[i + 1]}_lhs"] if i < len(ops) - 1 else Spec("RR").id))
