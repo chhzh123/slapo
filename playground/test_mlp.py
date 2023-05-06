67989502b8ac0cf91c24cc32f53a98c779e7bbdd
chhzh123 <hc676@cornell.edu>
2023-05-06 18:29:48 +0000

    Refactor with general names

--- playground/test_mlp.py
+++ playground/test_mlp.py
@@ -8,34 +8,50 @@ S: Sharded
 2: 10 SR
 """
 
-l1_input = z3.BitVec("L1_input", 2)
-l1_weight = z3.BitVec("L1_weight", 2)
-# l1_output = z3.BitVec("L1_output", 2)
-l2_input = z3.BitVec("L2_input", 2)
-l2_weight = z3.BitVec("L2_weight", 2)
-# l2_output = z3.BitVec("L2_output", 2)
+bitvecs = {}
 
-# input constraints
-input1_constraints = z3.Or(
-    z3.And(l1_input == 0, l1_weight == 1),
-        z3.Or(z3.And(l1_input == 1, l1_weight == 2),
-            z3.And(l1_input == 2, l1_weight == 0)))
+ops = ["matmul", "matmul_1"]
 
-# output constraints
-l1_output = z3.If(l1_input == 0, 1,
-    z3.If(l1_input == 1, 0, 2))
-l2_output = z3.If(l2_input == 0, 1,
-    z3.If(l2_input == 1, 0, 2))
+class Spec:
+    def __init__(self, spec):
+        self.map = {
+            "RR": 0,
+            "RS": 1,
+            "SR": 2
+        }
+        if isinstance(spec, str):
+            self.spec = spec
+        else:
+            self.spec = list(self.map.keys())[list(self.map.values()).index(spec)]
 
-input2_constraints = z3.Or(
-    z3.And(l2_input == 0, l2_weight == 1),
-        z3.Or(z3.And(l2_input == 1, l2_weight == 2),
-            z3.And(l2_input == 2, l2_weight == 0)))
+    @property
+    def id(self):
+        return self.map[self.spec]
 
-# format constraints
-format_constraints = z3.And(z3.ULE(l1_input, 3), z3.ULE(l1_weight, 3), z3.ULE(l2_input, 3), z3.ULE(l2_weight, 3))
+    def __str__(self):
+        return self.spec
 
-goal = [input1_constraints, input2_constraints, format_constraints, l2_input != 2, l1_input == 0]
+input_constraints = []
+format_constraints = []
+for op in ops:
+    # no need to include output, since output can be obtained from inputs
+    lhs = z3.BitVec(f"{op}_lhs", 2) # input
+    rhs = z3.BitVec(f"{op}_rhs", 2) # weight
+    bitvecs[lhs] = lhs
+    bitvecs[rhs] = rhs
+
+    # input constraints
+    constraints = [z3.And(lhs == Spec("RR").id, rhs == Spec("RS").id),
+                  z3.And(lhs == Spec("RS").id, rhs == Spec("SR").id),
+                  z3.And(lhs == Spec("SR").id, rhs == Spec("RR").id)]
+    input_constraints.append(z3.Or(*constraints))
+
+    # format constraints
+    format_constraints.extend([z3.ULE(lhs, 3), z3.ULE(rhs, 3)])
+
+goal = []
+goal += input_constraints
+goal += format_constraints
 
 def solve(phi):
     sol = z3.Solver()
@@ -52,11 +68,7 @@ def model_values(model):
 mod = solve(goal)
 print(mod)
 results = model_values(mod)
-for key, value in results.items():
-    if value == 0:
-        value = "RR"
-    elif value == 1:
-        value = "RS"
-    elif value == 2:
-        value = "SR"
-    print(f"{key} = {value}")
+for op in ops:
+    lhs = results[f"{op}_lhs"]
+    rhs = results[f"{op}_rhs"]
+    print(f"{op}: {Spec(lhs)} x {Spec(rhs)}")
