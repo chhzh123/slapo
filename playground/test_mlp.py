3f9ec7474737b51f681e121acde1021e132b81b7
chhzh123 <hc676@cornell.edu>
2023-05-06 17:52:37 +0000

    Fix format constraints

--- playground/test_mlp.py
+++ playground/test_mlp.py
@@ -1,6 +1,8 @@
 import z3
 
 """
+R: Replicated
+S: Sharded
 0: 00 RR
 1: 01 RS
 2: 10 SR
@@ -8,10 +10,10 @@ import z3
 
 l1_input = z3.BitVec("L1_input", 2)
 l1_weight = z3.BitVec("L1_weight", 2)
-l1_output = z3.BitVec("L1_output", 2)
-# l2_input = z3.BitVec("L2_input", 2)
+# l1_output = z3.BitVec("L1_output", 2)
+l2_input = z3.BitVec("L2_input", 2)
 l2_weight = z3.BitVec("L2_weight", 2)
-l2_output = z3.BitVec("L2_output", 2)
+# l2_output = z3.BitVec("L2_output", 2)
 
 # input constraints
 input1_constraints = z3.Or(
@@ -20,7 +22,7 @@ input1_constraints = z3.Or(
             z3.And(l1_input == 2, l1_weight == 0)))
 
 # output constraints
-l1_output = l2_input = z3.If(l1_input == 0, 1,
+l1_output = z3.If(l1_input == 0, 1,
     z3.If(l1_input == 1, 0, 2))
 l2_output = z3.If(l2_input == 0, 1,
     z3.If(l2_input == 1, 0, 2))
@@ -31,11 +33,9 @@ input2_constraints = z3.Or(
             z3.And(l2_input == 2, l2_weight == 0)))
 
 # format constraints
-# format_constraints = z3.And(l1_input < 3, l1_weight < 3, l1_output < 3, l2_input < 3, l2_weight < 3, l2_output < 3)
+format_constraints = z3.And(z3.ULE(l1_input, 3), z3.ULE(l1_weight, 3), z3.ULE(l2_input, 3), z3.ULE(l2_weight, 3))
 
-# goal = z3.And(input_constraints, format_constraints)
-# goal = z3.And(input_constraints, l2_input < 3)
-goal = [input1_constraints, input2_constraints]
+goal = [input1_constraints, input2_constraints, format_constraints, l2_input != 2, l1_input == 0]
 
 def solve(phi):
     sol = z3.Solver()
@@ -50,6 +50,7 @@ def model_values(model):
     }
 
 mod = solve(goal)
+print(mod)
 results = model_values(mod)
 for key, value in results.items():
     if value == 0:
