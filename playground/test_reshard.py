cf6a49004abea44264801ad79ddfdce1f1d733be
chhzh123 <hc676@cornell.edu>
2023-05-07 21:57:31 +0000

    Add SR to RR

--- playground/test_reshard.py
+++ playground/test_reshard.py
@@ -56,17 +56,23 @@ if dist.get_rank() == 0:
 
 # 3rd implementation (async)
 st = time.time()
+splitted_tensor = torch.split(x, in_shape[-1] // p, dim=-1)
 for i in range(dist.get_world_size()):
-    send_tensor = torch.split(x, in_shape[-1] // p, dim=-1)[i].contiguous()
+    send_tensor = splitted_tensor[i].contiguous()
     if dist.get_rank() != i:
         dist.gather(send_tensor, dst=i, async_op=True)
-    else:
-        gather_list = [
-            torch.empty((1, 1), dtype=x.dtype, device=x.device)
-            for _ in range(dist.get_world_size())
-        ]
-        dist.gather(send_tensor, gather_list, dst=i)
-        ret = torch.cat(gather_list, dim=0)
-        print(ret)
+gather_list = [
+    torch.empty((1, 1), dtype=x.dtype, device=x.device)
+    for _ in range(dist.get_world_size())
+]
+dist.gather(splitted_tensor[dist.get_rank()], gather_list, dst=dist.get_rank())
+ret = torch.cat(gather_list, dim=0)
+print(ret)
 if dist.get_rank() == 0:
     print("3rd", time.time() - st)
+
+# test SR->RR
+outshape = (3, 3)
+ret = torch.empty(outshape, dtype=x.dtype, device=x.device)
+dist.all_gather_into_tensor(ret, x)
+print(ret)
