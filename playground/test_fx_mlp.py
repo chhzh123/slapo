2dcd6a491454e5c4caa59f88bf4df23f8785ee41
chhzh123 <hc676@cornell.edu>
2023-05-18 15:14:12 +0000

    Add tests

--- playground/test_fx_mlp.py
+++ playground/test_fx_mlp.py
@@ -11,63 +11,76 @@ import slapo
 import torch.distributed as dist
 
 
+d = 1024
+seq = 512
+
 class MLP(nn.Module):
     def __init__(self):
         super().__init__()
-        self.fc1 = nn.Linear(1024, 4096)
-        self.fc2 = nn.Linear(4096, 1024)
+        self.fc1 = nn.Linear(d, 4 * d)
+        self.fc2 = nn.Linear(4 * d, d)
 
     def forward(self, x):
-        beg = time.time()
+        # beg = time.time()
         x = self.fc1(x)
-        if dist.get_rank() == 0:
-            print(f"fc1: {time.time() - beg}")
+        # if dist.get_rank() == 0:
+        #     print(f"fc1: {time.time() - beg}")
         x = F.relu(x)
-        beg = time.time()
+        # beg = time.time()
         x = self.fc2(x)
-        if dist.get_rank() == 0:
-            print(f"fc2: {time.time() - beg}")
+        # if dist.get_rank() == 0:
+        #     print(f"fc2: {time.time() - beg}")
         return x
 
 
-# from solver import Solver
+from solver import Solver
 
-# sol = Solver(fx.symbolic_trace(MLP()), p=2)
-# sol.solve([torch.randn(512, 1024)])
+sol = Solver(fx.symbolic_trace(MLP()), p=8)
+sol.solve([torch.randn(seq, d)])
+sys.exit()
 
-dist.init_process_group("nccl", world_size=8)
+dist.init_process_group("nccl", world_size=4)
 torch.cuda.set_device(dist.get_rank())
 device = f"cuda:{dist.get_rank()}"
 
 
 def reshard_RS_to_SR(_module, _input, output):
-    beg = time.time()
+    # beg = time.time()
+    # in_tensor = output
+    # in_shape = in_tensor.shape
+    # chunk_shape = list(in_shape[:-2]) + [in_shape[-2] // sch.world_size, in_shape[-1]]
+    # splitted_tensor = torch.split(in_tensor, in_shape[-2] // sch.world_size, dim=-2)
+    # comm_start = time.time()
+    # for i in range(dist.get_world_size()):
+    #     in_time = time.time()
+    #     send_tensor = splitted_tensor[i].contiguous()
+    #     if dist.get_rank() != i:
+    #         dist.gather(send_tensor, dst=i, async_op=True)  # send
+    #     else:
+    #         gather_list = [
+    #             torch.empty(chunk_shape, dtype=in_tensor.dtype, device=in_tensor.device)
+    #             for _ in range(dist.get_world_size())
+    #         ]
+    #         handle = dist.gather(send_tensor, gather_list, dst=i, async_op=True)  # recv
+    #     if dist.get_rank() == 0:
+    #         print(f"in_comm{i}: {time.time() - in_time}")
+    # if dist.get_rank() == 0:
+    #     print(f"comm0: {time.time() - comm_start}")
+    # handle.wait()
+    # if dist.get_rank() == 0:
+    #     print(f"comm1: {time.time() - comm_start}")
+    # ret = torch.cat(gather_list, dim=-1)
+    # if dist.get_rank() == 0:
+    #     print(f"shard: {time.time() - beg}")
     in_tensor = output
-    in_shape = in_tensor.shape
-    chunk_shape = list(in_shape[:-2]) + [in_shape[-2] // sch.world_size, in_shape[-1]]
-    splitted_tensor = torch.split(in_tensor, in_shape[-2] // sch.world_size, dim=-2)
-    comm_start = time.time()
-    for i in range(dist.get_world_size()):
-        in_time = time.time()
-        send_tensor = splitted_tensor[i].contiguous()
-        if dist.get_rank() != i:
-            dist.gather(send_tensor, dst=i, async_op=True)  # send
-        else:
-            gather_list = [
-                torch.empty(chunk_shape, dtype=in_tensor.dtype, device=in_tensor.device)
-                for _ in range(dist.get_world_size())
-            ]
-            handle = dist.gather(send_tensor, gather_list, dst=i, async_op=True)  # recv
-        if dist.get_rank() == 0:
-            print(f"in_comm{i}: {time.time() - in_time}")
-    if dist.get_rank() == 0:
-        print(f"comm0: {time.time() - comm_start}")
-    handle.wait()
-    if dist.get_rank() == 0:
-        print(f"comm1: {time.time() - comm_start}")
-    ret = torch.cat(gather_list, dim=-1)
-    if dist.get_rank() == 0:
-        print(f"shard: {time.time() - beg}")
+    temp = in_tensor
+    # temp = temp.contiguous()
+    gather_shape = list(temp.shape)
+    gather_shape[-1] = dist.get_world_size() * gather_shape[-1]
+    ret = torch.empty(gather_shape, dtype=temp.dtype, device=in_tensor.device)
+    dist.all_gather_into_tensor(ret, temp)
+    # ret = ret.contiguous()
+    ret = ret.split(ret.shape[-2] // dist.get_world_size(), dim=-2)[dist.get_rank()]
     return ret
 
 
@@ -87,6 +100,7 @@ mlp = MLP()
 mlp_reshard = copy.deepcopy(mlp).to(device=device)
 sch = slapo.create_schedule(mlp_reshard)
 print(sch.mod)
+# with slapo.Verify(sch, [torch.randn(8, seq, d).to(device=device)]):
 sch["fc1"].shard("weight", axis=0)
 sch["fc1"].shard("bias", axis=0)
 # sch["fc2"].shard("weight", axis=1)
@@ -105,15 +119,15 @@ sch["fc2"].sync("fwd_post", sync_op_or_fn="all_reduce")
 mod_mega, _ = slapo.build(sch)
 
 start = time.time()
-for _ in range(100):
-    mod_reshard(torch.randn(8, 512, 1024).to(device=device))
+for _ in range(10):
+    mod_reshard(torch.randn(8, seq, d).to(device=device))
 end = time.time()
 if dist.get_rank() == 0:
     print("reshard", end - start)
 
 start = time.time()
-for _ in range(100):
-    mod_mega(torch.randn(8, 512, 1024).to(device=device))
+for _ in range(10):
+    mod_mega(torch.randn(8, seq, d).to(device=device))
 end = time.time()
 if dist.get_rank() == 0:
     print("mega", end - start)
--- playground/test_layer_hf.py
+++ playground/test_layer_hf.py
@@ -4,16 +4,19 @@
 from transformers import BertLMHeadModel, AutoConfig
 import inspect
 import torch
+import slapo
+import time
+from solver import Solver
 
 config = AutoConfig.from_pretrained("bert-large-uncased")
 model = BertLMHeadModel(config)
 print(config)
 
-import slapo
-
 sch = slapo.create_schedule(model)
 input_names = ["hidden_states"]
-subsch = sch["bert.encoder.layer.0"]
+tot_time = 0
+i = 0
+subsch = sch[f"bert.encoder.layer.{i}"]
 sig = inspect.signature(subsch.mod.forward)
 concrete_args = {
     p.name: p.default for p in sig.parameters.values() if p.name not in input_names
@@ -26,7 +29,37 @@ subsch.trace(
 )
 print(subsch.mod.graph)
 
-from solver import Solver
-
-sol = Solver(subsch.mod, p=2)
+st = time.time()
+sol = Solver(subsch.mod, p=8)
 sol.solve([torch.randn(8, 512, 1024)])
+tot_time += time.time() - st
+print("Time: ", tot_time)
+
+
+# from transformers import AutoConfig, GPTNeoModel
+# config = AutoConfig.from_pretrained("EleutherAI/gpt-neo-1.3B")
+# model = GPTNeoModel(config)
+# print(config)
+
+# sch = slapo.create_schedule(model)
+# input_names = ["hidden_states"]
+# tot_time = 0
+# for i in range(config.num_layers):
+#     subsch = sch[f"h.{i}.mlp"]
+#     sig = inspect.signature(subsch.mod.forward)
+#     concrete_args = {
+#         p.name: p.default for p in sig.parameters.values() if p.name not in input_names
+#     }
+#     subsch.trace(
+#         recursive=False,
+#         flatten=True,
+#         tracer="pytorch",
+#         concrete_args=concrete_args,
+#     )
+#     print(subsch.mod.graph)
+
+#     st = time.time()
+#     sol = Solver(subsch.mod, p=8)
+#     sol.solve([torch.randn(8, 1024, 2048)])
+#     tot_time += time.time() - st
+# print("Time: ", tot_time)
