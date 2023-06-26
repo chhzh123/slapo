195bd4e3de7905000055fe283fe543bf8f3366b3
chhzh123 <hc676@cornell.edu>
2023-06-26 17:47:54 +0000

    Add nsys

---
+++ playground/benchmark/nsys.sh
@@ -0,0 +1,2 @@
+nsys profile -w true -t cuda,nvtx,osrt,cudnn,cublas -s none -o nsight_report -f true --capture-range=cudaProfilerApi --capture-range-end=stop --cudabacktrace=true --osrt-threshold=10000 -x true torchrun --nproc_per_node 8 bench_slapo_inference.py --name llama
+# sudo sh -c 'echo 1 >/proc/sys/kernel/perf_event_paranoid'
