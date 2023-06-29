e700d2b881fe2e90f936212d0a6e676460b5282d
chhzh123 <hc676@cornell.edu>
2023-06-29 19:56:50 +0000

    Add DS test

--- playground/benchmark/bench_slapo_inference.py
+++ playground/benchmark/bench_slapo_inference.py
@@ -6,7 +6,6 @@ from argparse import ArgumentParser
 
 import torch
 import torch.distributed as dist
-
 import slapo
 from utils import perf_model, get_model
 from bert_schedule import schedule_bert
@@ -17,6 +16,8 @@ parser.add_argument("--name", required=True, type=str, help="model_name")
 parser.add_argument("--bs", default=1, type=int, help="batch size")
 parser.add_argument("--max_seq_len", default=1024, type=int, help="sequence length")
 parser.add_argument("--nsys", default=False, type=bool, help="Use nsys to profile")
+# import deepspeed
+# parser.add_argument("--local_rank", default=0, type=int, help="local rank")
 args = parser.parse_args()
 
 bs = args.bs
@@ -37,6 +38,7 @@ if __name__ == "__main__":
     # https://pytorch.org/docs/stable/notes/cuda.html#id5
     os.environ["NCCL_ASYNC_ERROR_HANDLING"] = "0"
     dist.init_process_group("nccl", world_size=int(os.environ["WORLD_SIZE"]))
+    # deepspeed.init_distributed()
     torch.cuda.set_device(dist.get_rank())
 
     mod, config, seq_len = get_model(
@@ -56,7 +58,7 @@ if __name__ == "__main__":
     perf_model(
         mod,
         input_ids,
-        use_cuda_graph=True if args.name == "bert" else False,
+        use_cuda_graph=False if args.name == "bert" else False,
         nsys=args.nsys,
     )
     del mod
