ff3e2c484fe163447f88ece3a6bfb91a1360fe86
chhzh123 <hc676@cornell.edu>
2023-08-10 18:44:35 +0000

    Update tune

--- benchmark/tune_single_node.py
+++ benchmark/tune_single_node.py
@@ -7,13 +7,13 @@ models = ["bert", "roberta", "gpt", "opt", "t5", "wideresnet"]
 impls = ["slapo-deepspeed", "slapo-megatron", "deepspeed", "megatron"]
 
 model_name_mapping = {
-    "bert": "bert-large-uncased",
-    "roberta": "roberta-large",
+    "bert": "bert-xlarge",
+    "roberta": "roberta-xlarge",
     "albert": "albert-large-v2",
-    "gpt": "EleutherAI/gpt-neo-1.3B",
+    "gpt_neo": "EleutherAI/gpt-neo-2.7B",
     "opt": "facebook/opt-350m",
-    "t5": "t5-large",
-    "wideresnet": "wideresnet-250M",
+    "t5": "t5-3b",
+    "wideresnet": "wideresnet-2.4B",
 }
 
 for model in models:
@@ -33,7 +33,7 @@ for model in models:
                 ckpt_ratio = "ckpt_ratio"
             cmd = "python3 -m slapo.autotune.tune"
             cmd += f" --config {os.getcwd()}/../examples/{model}/tune_cfg.py"
-            cmd += f" --db results/{model}-gpu{n_gpu}-{impl}.json"
+            cmd += f" --db results/{model}-gpu{n_gpu}-{seq_len}-{impl}.json"
             cmd += f" --error-stop symbol"
             cmd += f"  bench_single_node.py {impl}"
             cmd += f" --model {model_name_mapping[model]} --gpus {n_gpu} --seq-len {seq_len}"
